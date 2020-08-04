8878070be2213f3685d7ddfac095d03d7a52b806
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-04 14:50:13 +0200

    Create Hero cmpnnt, render in List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h2 className={styles.subtitle}>Dowolna treść </h2>
+        <Hero />
       </section>
     )
   }
