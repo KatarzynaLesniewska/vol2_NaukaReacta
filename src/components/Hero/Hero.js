f94109164e8998579a3c7ea7979e0cdc26d7ad0c
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-04 16:22:51 +0200

    Check if prop works

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import styles from './Hero.scss';
 
 const Hero = (props) => (
     <header className={styles.component}>
-        <h2 className={styles.title}>"Things to do"</h2>
+        <h2 className={styles.title}>{props.titleText}</h2>
         <img className={styles.image} src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' />
     </header>
 );
