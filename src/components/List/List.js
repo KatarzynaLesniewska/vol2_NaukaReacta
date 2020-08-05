8839e5358533fa9bc95b2e41974299c94f826cf7
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-05 17:37:30 +0200

    Install propTpes & define them in List cmpnnt at the begining of List class

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: propTypes.node,
+    children: propTypes.node,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
