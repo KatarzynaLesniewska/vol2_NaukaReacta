5ca306d7f8c0c5d632a6079ef9c83dbeda1d8ccb
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-06 19:35:06 +0200

    Add propType definition of prop titleText in Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = (props) => (
     <header className={styles.component}>
@@ -7,5 +8,9 @@ const Hero = (props) => (
         <img className={styles.image} src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' />
     </header>
 );
+
+Hero.propTypes = {
+    titleText: PropTypes.node,
+};
 
 export default Hero;
