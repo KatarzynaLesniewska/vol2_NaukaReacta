d1d2b0964ecda0fa7034ffaa4f3efba4d61a857d
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-06 21:24:30 +0200

    Pass img's url frm App trough List to Hero& define types of these props

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types';
 const Hero = (props) => (
     <header className={styles.component}>
         <h2 className={styles.title}>{props.titleText}</h2>
-        <img className={styles.image} src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' />
+        <img className={styles.image} src={props.heroImgURL} />
     </header>
 );
 
 Hero.propTypes = {
     titleText: PropTypes.node.isRequired,
+    heroImgURL: PropTypes.text,
 };
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,16 +7,21 @@ class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
     children: PropTypes.node,
+    heroUrlIMG: PropTypes.text,
   }
   
   static defaultProps = {
     children: <p>I can do all the things!!!</p>,
+    heroUrlIMG: <p>Img got lost somewhere :( </p>,
   }
 
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} />
+        <Hero 
+        titleText={this.props.title} 
+        heroImgURL={this.props.heroUrlIMG}
+        />
         <div className={styles.description}>
           {this.props.children}
         </div>
