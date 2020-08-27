a1f54d80e939cdcff213f5f8ce686d875783944b
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 22:09:01 +0200

    Debug image in dataStore, swap with heroImgURL

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -18,7 +18,7 @@ export const pageContents = {
 export const listData = {
   title: 'Things to do <sup>soon!</sup>',
   description: 'Interesting things I want to check out!',
-  image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  heroImgURL: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   columns: [
     {
       key: 0,
