ef39312a104ccfde0325e9e283f64c291d6743b2
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 17:18:45 +0200

    Use list Data from dataStore in <List {...listData} /> in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,9 +9,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]} heroUrlIMG={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}>
-          <p>Interesting things I want to check out!</p>
-        </List>
+        <List {...listData} /> 
       </main>
     )
   }
