cccaa915ef0f93fc3e60e80209a54680d8e26f49
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 17:10:56 +0200

    Imoprt {pageContents} from dataStore to App & swap them to {pageContents.title} {pageContents.subtitle}

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>A simple to-do app, with lists, columns and cards</h2>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
         <List title={['Things to do ', <sup key='1'>soon!</sup>]} heroUrlIMG={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}>
           <p>Interesting things I want to check out!</p>
         </List>
