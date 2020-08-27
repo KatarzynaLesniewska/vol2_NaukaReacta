cf52145ddc9b120383685c927deffceede219381
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 20:40:07 +0200

    Creat card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+    static propTypes = {
+        title: PropTypes.PropTypes.text,
+    }
+
+  render() {
+    return (
+        <section className={styles.component}>
+            <h3 className={styles.title}>
+                {this.props.title}
+            </h3>
+        </section>
+    )
+  }
+}
+
+export default Card;
