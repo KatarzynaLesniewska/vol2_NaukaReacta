eacd5bec4fc0579efc474715f01ab0ad92fa4f5a
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 20:07:12 +0200

    Import Creator to List and add new <div> to JSX List with <Creator text, action>

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore'; 
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
@@ -34,6 +35,9 @@ class List extends React.Component {
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
           ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
