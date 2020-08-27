1d97772ed89ceafe21ef0004fbfa0549850fb5fa
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 19:13:14 +0200

    Swap 3x<Cloumn> in List with 1x<Column> 'looped' with this.state.columns.map method

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -31,9 +31,9 @@ class List extends React.Component {
           {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-          <Column title={'Animals'}></Column>
-          <Column title={'Plants'}></Column>
-          <Column title={'Minerals'}></Column>
+          {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
       </section>
     )
