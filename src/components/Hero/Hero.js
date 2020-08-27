dc49dc03c1e4cfc89d30ea9daa41297209211127
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 18:06:14 +0200

    Pass props.titleText as an argument to ReactHtmlPa rser in <h2> in Hero to parese it

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import ReactHtmlParser from 'react-html-parser';
 
 const Hero = (props) => (
     <header className={styles.component}>
-        <h2 className={styles.title}>{props.titleText}</h2>
+        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
         <img className={styles.image} src={props.heroImgURL} />
     </header>
 );
