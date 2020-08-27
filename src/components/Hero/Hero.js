442ebd98b2419dea2b157c2f8a4d9a4e41332d80
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 18:12:40 +0200

    Debug heroImgURL: PropTypes.text, to .string, in Hero.propTypes = {} def in Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
 
 Hero.propTypes = {
     titleText: PropTypes.node.isRequired,
-    heroImgURL: PropTypes.text,
+    heroImgURL: PropTypes.string,
 };
 
 export default Hero;
