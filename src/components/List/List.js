e7f6b363b367de5b7a496145ad7b202687d739a6
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 18:16:56 +0200

    Import ReactHtmlParser from 'react-html-parser'; to  List & parse {this.props.description} ->  {ReactHtmlParser(this.props.description)}

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore'; 
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
@@ -23,7 +24,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} heroImgURL={this.props.heroUrlIMG} />
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column title={'Animals'}></Column>
