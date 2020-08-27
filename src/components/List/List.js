651a47eca14ffd56ee4bc9d811887056696a1e8f
Kasia Leśniewska <klesniewska29@gmail.com>
2020-08-27 17:43:32 +0200

    Swap children with description in static defaultProps in List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -14,7 +14,7 @@ class List extends React.Component {
   }
   
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
     heroUrlIMG: <p>Img got lost somewhere :( </p>,
   }
 
