ef7f0684e9a61d5bc3fec9822927359a2b054cda
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-07 18:58:42 +0200

    Debug Icon.js icon & name

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+
+Icon.propTypes = {
+  icon: PropTypes.node,
+  name: PropTypes.node,
+};
 
 export default Icon;
