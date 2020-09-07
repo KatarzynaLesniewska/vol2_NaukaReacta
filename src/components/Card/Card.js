2ef671b2cfa932011042fe320b75d7e0e10fb37a
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-07 19:32:12 +0200

    tutah Twoja wiadomosc

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,19 +3,19 @@ import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-    static propTypes = {
-        title: PropTypes.PropTypes.string,
-    }
-
-  render() {
-    return (
-        <section className={styles.component}>
-            <h3 className={styles.title}>
-                {this.props.title}
-            </h3>
-        </section>
-    )
+  static propTypes = {
+    title: PropTypes.PropTypes.string,
   }
+};
+
+render() {
+  return (
+    <section className={styles.component}>
+      <h3 className={styles.title}>
+        {this.props.title}
+      </h3>
+    </section>
+  );
 }
 
 export default Card;
