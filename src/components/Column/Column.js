25e2d4fb52ad765f9cd15f71ef2ffb3f6b81cbbb
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-07 19:08:53 +0200

    Debug Column.js by adding poptypes of cards and ic on?

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,56 +1,58 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore'; 
+import { settings } from '../../data/dataStore';
 import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
 
 
 class Column extends React.Component {
-    state = {
-        cards: this.props.cards || [],
-    }
+  state = {
+    cards: this.props.cards || [],
+  }
 
-    static propTypes = {
-        title: PropTypes.PropTypes.string,
-    }
+  static propTypes = {
+    title: PropTypes.PropTypes.string,
+    cards: this.propTypes.node,
+    icon: this.propTypes.node,
+  }
 
-    addCard(title){
-        this.setState(state => (
+  addCard(title) {
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
           {
-            cards: [
-              ...state.cards,
-              {
-                key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-                title,
-                icon: 'list-alt',
-                cards: []
-              }
-            ]
-          }
-        ));
+            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: [],
+          },
+        ],
       }
+    ));
+  }
 
   render() {
     return (
-        <section className={styles.component}>
-            <h3 className={styles.title}>
-                <span className={styles.icon}>
-                    <Icon name={this.props.icon} />
-                </span>
-                {this.props.title}
-            </h3>
-            <div className={styles.cards}>
-                {this.state.cards.map(({key, ...cardProps}) => (
-                    <Card key={key} {...cardProps} />
-                ))}
-            </div>
-            <div className={styles.creator}>
-                <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-            </div>
-        </section>
-    )
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
+          {this.props.title}
+        </h3>
+        <div className={styles.cards}>
+          {this.state.cards.map(({ key, ...cardProps }) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
+      </section>
+    );
   }
 }
 
