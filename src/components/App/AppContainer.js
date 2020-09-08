2ae979810dd92ec9eab8e135fed46b62cd7fb5d3
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-08 23:58:45 +0200

    Add 'lists: state.lists,' to const mapStateToProps with no effect, doesn't render/ work

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -3,6 +3,8 @@ import App from './App';
 
 const mapStateToProps = state => ({
   title: state.app.title,
+  subtitle: state.app.subtitle,
+  lists: state.lists,
 });
 
 export default connect(mapStateToProps)(App);
