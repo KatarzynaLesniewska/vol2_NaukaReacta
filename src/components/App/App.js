884987a13d55c2b710f80f1a0a95f1981655a104
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-08 23:10:35 +0200

    Comment <List/> & //import List, change whole rend er() {} code bc pageContents, comment //pageContents, add propTypes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js';
-import {pageContents, listData} from '../../data/dataStore';
+import PropTypes from 'prop-types';
+// import List from '../List/List.js';
+// import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+  }
+
   render() {
+    const {title, subtitle} = this.props;
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List {...listData} /> 
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {/*
+        <List {...listData} />
+        */}
       </main>
     );
   }
