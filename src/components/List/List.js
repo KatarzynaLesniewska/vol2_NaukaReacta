935a18daef3c79116452c7ac579e9f9051df8346
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-09 00:16:28 +0200

    Add declaration of props in render, change all 3 t his.props.title -> title and so on

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-//import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore'; 
 import ReactHtmlParser from 'react-html-parser';
+//import Column from '../Column/Column.js';
 //import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
@@ -21,11 +21,13 @@ class List extends React.Component {
   }
 
   render() {
+    const {title, heroUrlIMG, description} = this.props;
+
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} heroImgURL={this.props.heroUrlIMG} />
+        <Hero titleText={title} heroImgURL={heroUrlIMG} />
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(description)}
         </div>
         {/*
         <div className={styles.columns}>
