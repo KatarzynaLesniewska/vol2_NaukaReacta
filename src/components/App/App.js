24fc60b1460f87de24a665800bfe2adb302ee9e1
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-09 00:29:57 +0200

    Add imoprt List in App, add lists to propTypes, to const in render and change {...listData} in return () to lists.map...

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
-// import List from '../List/List.js';
+import List from '../List/List.js';
 // import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
 
   render() {
-    const {title, subtitle} = this.props;
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        {/*
-        <List {...listData} />
-        */}
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
       </main>
     );
   }
