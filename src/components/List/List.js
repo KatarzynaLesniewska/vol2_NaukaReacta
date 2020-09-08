2c771ca656e478db9150c6bf5ff0fff357e9f8f4
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-09 00:08:35 +0200

    Comment {/**/} <Column/>, <Creator/>, //import Col umn/Creator, delete state & addColumn

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,16 +2,12 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+//import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore'; 
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
-
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
@@ -24,22 +20,6 @@ class List extends React.Component {
     heroUrlIMG: <p>Img got lost somewhere :( </p>,
   }
 
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
     return (
       <section className={styles.component}>
@@ -47,6 +27,7 @@ class List extends React.Component {
         <div className={styles.description}>
           {ReactHtmlParser(this.props.description)}
         </div>
+        {/*
         <div className={styles.columns}>
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
@@ -55,6 +36,7 @@ class List extends React.Component {
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
+        */}
       </section>
     );
   }
