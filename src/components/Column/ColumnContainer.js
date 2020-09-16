9070beb5287da3a3d7574b58dd74646d60f18b56
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-16 20:53:31 +0200

    Change ColumnContainer.js; delete 'getCardsForColu mn'; import getCardsForColumn, createActionAddCard; add mapDispatchToPro export default connect(mapStateToProps, mapDispatchToProps)(Column); ps; export

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,10 +1,20 @@
 import {connect} from 'react-redux';
 import Column from './Column';
+import {getCardsForColumn, createActionAddCard} from '../../redux/columnsRedux.js';
 
+{/*
 export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+*/} 
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForColumn(state, props.id),
+}); 
+
+const mapDispatchToProps = (dispatch, props) => ({
+  addCard: title => dispatch(createActionAddCard({
+    listId: props.id,
+    title,
+  })),
 });
 
-export default connect(mapStateToProps)(Column);
+export default connect(mapStateToProps, mapDispatchToProps)(Column);
