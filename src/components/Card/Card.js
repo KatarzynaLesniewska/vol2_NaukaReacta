2f36d1b8a77e402c866862bbd3804c357a47893e
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-16 21:00:30 +0200

    Change Card.js; add defaultProps icon; add icon to propTypes, const, <span><Icon>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
+import Icon from '../Icon/Icon.js';
+import { settings } from '../../data/dataStore';
 
 class Card extends React.Component {
   static propTypes = {
     title: PropTypes.PropTypes.string,
+    icon: PropTypes.string,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultCardIcon,
   }
 
   render() {
-    const {title} = this.props;
+    const {title, icon} = this.props;
 
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={icon} />
+          </span>
           {title}
         </h3>
       </section>
