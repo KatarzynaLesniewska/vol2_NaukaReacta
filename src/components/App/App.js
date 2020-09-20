f7f36406f8dd0e6f397556ce80869893a61c82e3
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-20 20:19:32 +0200

    Add /info subpage (and /about)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import About from '../About/About';
+
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/info' component={Info} />
+      <Route exact path='/about' component={About} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
