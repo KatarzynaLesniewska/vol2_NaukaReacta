0af446c14bd256ab0aebcd93e669350e56c91e78
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-20 20:26:00 +0200

    Debug (about subpage - delete)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import About from '../About/About';
-
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/info' component={Info} />
-      <Route exact path='/about' component={About} />
     </Switch>
   </BrowserRouter>
 );
