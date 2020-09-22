be6c755a82d4bd34db8949f24e058a0e13243e78
Kasia Leśniewska <klesniewska29@gmail.com>
2020-09-22 22:58:20 +0200

    Debug import of Header in MainLayout

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from './../Header/Header.js';
+import Header from '../Header/Header';
+//import Header from './../Header/Header.js';
 
 const MainLayout = ({children}) => (
   <div>
